2a6a08629b7ebf21afa0fa78b664a036b84b4696
szorowi1 <szorowi1@gmail.com>
2021-11-06 15:59:36 -0400

    item-#3 (4) - update eat26 - update gad7 - update gse

--- tasks/self-report/surveys/gad7.js
+++ tasks/self-report/surveys/gad7.js
@@ -1,10 +1,15 @@
-# Generalized Anxiety Disorder Scale (GAD-7)
-
-```javascript
+/**
+* Generalized Anxiety Disorder Scale (GAD-7)
+*
+* Spitzer, R. L., Kroenke, K., Williams, J. B., & Löwe, B. (2006). A brief measure
+* for assessing generalized anxiety disorder: the GAD-7. Archives of internal
+* medicine, 166(10), 1092-1097.
+*
+**/
 
 var gad7 = {
   type: 'survey-template',
-  items = [
+  items: [
     "Feeling nervous, anxious, or on edge",
     "Not being able to stop or control worrying",
     "Worrying too much about different things",
@@ -13,18 +18,17 @@ var gad7 = {
     "Becoming easily annoyed or irritable",
     "Feeling afraid as if something awful might happen"
   ],
-  scale = [
+  scale: [
     "Not at all",
     "Several days",
     "Over half the days",
     "Nearly every day"
   ],
-  reverse = [
+  reverse: [
     false, false, false, false, false, false, false
   ],
-  instructions = 'Over the last 2 weeks, how often have you been bothered by the following problems?';
+  instructions: 'Over the last 2 weeks, how often have you been bothered by the following problems?',
+  survey_width: 950,
+  item_width: 40,
+  scale_repeat: 7,
 }
-```
-
-## References
-- Spitzer, R. L., Kroenke, K., Williams, J. B., & Löwe, B. (2006). A brief measure for assessing generalized anxiety disorder: the GAD-7. Archives of internal medicine, 166(10), 1092-1097.
