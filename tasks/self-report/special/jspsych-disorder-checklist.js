90e55630488a3ed5d857319cd6133c113921e483
szorowi1 <szorowi1@gmail.com>
2022-04-06 09:31:32 -0400

    issue-#32 (3) - full working draft

--- tasks/self-report/special/jspsych-disorder-checklist.js
+++ tasks/self-report/special/jspsych-disorder-checklist.js
@@ -23,7 +23,7 @@ jsPsych.plugins['disorder-checklist'] = (function() {
     const disorders = [
       "Major depressive disorder",
       "Seasonal affective disorder",
-      "Bipolar disorder (1 or 2)",
+      "Bipolar disorder (type 1 or 2)",
       "Generalized anxiety disorder",
       "Posttraumatic stress disorder",
       "Obsessive-compulsive disorder",
@@ -44,21 +44,44 @@ jsPsych.plugins['disorder-checklist'] = (function() {
 
     // Inject CSS
     html += `<style>
-      #searchBar {
+
+      /* Import Google material icons */
+      @import url("https://fonts.googleapis.com/icon?family=Material+Icons");
+
+      .jspsych-content {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .checklist-container {
+        position: relative;
+        width: 462px;
+        white-space: nowrap;
+      }
+
+      .searchbar-container {
+        position: relative;
+      }
+
+      #searchbar {
         width: 400px;
-        background-image: url('/css/searchicon.png'); /* Add a search icon to input */
-        background-position: 10px 12px; /* Position the search icon */
-        background-repeat: no-repeat; /* Do not repeat the icon image */
         font-size: 16px; /* Increase font-size */
         padding: 12px 20px 12px 40px; /* Add some padding */
         border: 1px solid #ddd; /* Add a grey border */
-        margin-bottom: 12px; /* Add some space below the input */
+        margin-bottom: 4px; /* Add some space below the input */
       }
 
-      #disorderList {
-        height: 400px;
-        width: 400px;
-        /* Remove default list styling */
+      .searchbar-container span {
+        position: absolute;
+        left: 12px;
+        top: 10px;
+        font-family: "Material Icons";
+      }
+
+      #disorder-list {
+        height: 375px;
         list-style-type: none;
         padding: 0;
         margin: 0;
@@ -66,43 +89,80 @@ jsPsych.plugins['disorder-checklist'] = (function() {
         overflow-y:scroll;
       }
 
-      #disorderList li a {
+      #disorder-list li a {
         border: 1px solid #ddd; /* Add a border to all links */
         margin-top: -1px; /* Prevent double borders */
         background-color: #f6f6f6; /* Grey background color */
         padding: 12px; /* Add some padding */
         text-decoration: none; /* Remove default text underline */
-        font-size: 18px; /* Increase the font-size */
+        font-size: 17px; /* Increase the font-size */
         color: black; /* Add a black text color */
         display: block; /* Make it into a block element to fill the whole list */
       }
 
-      #disorderList li a:hover:not(.header) {
-        background-color: #eee; /* Add a hover effect to all links, except for headers */
+      #disorder-list li a:hover:not(.header) {
+        background-color: #eee; /* Add a hover effect to all links */
       }
 
-      #disorderList li a:active {
-        filter: brightness(1.2);
+      #disorder-list li a:active {
+        filter: brightness(1.1); /* Add brightness effect on click */
       }
 
-      #disorderList li a[status="selected"] {
-        background-color: #909090;
+      #disorder-list li a[status="selected"] {
+        background-color: #7db5ca; /* Change color of selected items */
       }
 
-      #disorderList li a[status="selected"]:hover:not(.header) {
-        background-color: #808080;
+      #disorder-list li a[status="selected"]:hover:not(.header) {
+        background-color: #6babc2; /* Add a hover effect to selected links */
+      }
+
+      .checklist-footer {
+        margin: auto;
+        width: 462px;
+        padding: 0 0 0 0;
+        text-align: right;
+      }
+
+      .checklist-footer input[type=submit] {
+        background-color: #F0F0F0;
+        padding: 8px 20px;
+        border: none;
+        border-radius: 4px;
+        margin-top: 5px;
+        margin-bottom: 20px;
+        margin-right: 0px;
+        font-size: 16px;
+        color: black;
       }
 
     </style>`;
 
-    html += '<input type="text" id="searchBar" placeholder="Search for diagnoses..">';
+    // Add prompt.
+    html += '<p>Have you ever been diagnosed with any of the mental health conditions below?<br><small>Please select all that apply, or leave blank if you prefer not to stay.</small></p>'
+
+    // Open container.
+    html += '<div class="checklist-container">';
+
+    // Add searchbar.
+    html += '<div class="searchbar-container">';
+    html += '<input type="text" id="searchbar" placeholder="Search for diagnoses..">';
+    html += '<span class="material-icons-outlined">search</span>';
+    html += '</div>';
 
     // Add disorders to list.
-    html += '<ul id="disorderList">';
+    html += '<ul id="disorder-list">';
     disorders.forEach((d, i) => {
       html += '<li><a id="disorder-' + i + '" status="unselected" value="' + d + '">' + d + '</a></li>';
     });
     html += '</ul>';
+
+    // Add submit button.
+    html += '<div class="checklist-footer">';
+    html += `<input type="submit" id="submit" value="${trial.button_label}"></input>`;
+    html += '</div>';
+
+    // Close container.
+    html += '</div>';
 
     // Display HTML
     display_element.innerHTML = html;
@@ -125,19 +185,17 @@ jsPsych.plugins['disorder-checklist'] = (function() {
         } else {
           target.setAttribute('status', 'unselected');
         }
-        console.log(target)
       });
     });
-
 
     // Add event listener to search bar.
     display_element.querySelector('input').addEventListener('input', function(event) {
 
       // Declare variables
       var input, filter, ul, li, a, i, txtValue;
-      input = document.getElementById('searchBar');
+      input = document.getElementById('searchbar');
       filter = input.value.toUpperCase();
-      ul = document.getElementById("disorderList");
+      ul = document.getElementById("disorder-list");
       li = ul.getElementsByTagName('li');
 
       // Loop through all list items, and hide those who don't match the search query
@@ -153,30 +211,35 @@ jsPsych.plugins['disorder-checklist'] = (function() {
 
     });
 
-    // display_element.querySelector('#jspsych-survey-demo').addEventListener('submit', function(event) {
-    //
-    //   // Wait for response
-    //   event.preventDefault();
-    //
-    //   // Measure response time
-    //   var endTime = performance.now();
-    //   var response_time = endTime - startTime;
-    //
-    //   var question_data = serializeArray(this);
-    //   question_data = objectifyForm(question_data);
-    //
-    //   // Store data
-    //   var trialdata = {
-    //     "rt": response_time,
-    //   };
-    //
-    //   // Update screen
-    //   display_element.innerHTML = '';
-    //
-    //   // Move onto next trial
-    //   jsPsych.finishTrial(trialdata);
-    //
-    // });
+    display_element.querySelector('#submit').addEventListener('click', function(event) {
+
+      // Wait for response
+      event.preventDefault();
+
+      // Measure response time
+      var endTime = performance.now();
+      var response_time = endTime - startTime;
+
+      // Gather selections.
+      var diagnoses = [];
+      selected = display_element.querySelectorAll("[status=selected]");
+      selected.forEach(target => {
+        diagnoses.push(target.getAttribute('value'));
+      });
+
+      // Store data
+      var trialdata = {
+        diagnoses: diagnoses,
+        rt: response_time,
+      };
+
+      // Update screen
+      display_element.innerHTML = '';
+
+      // Move onto next trial
+      jsPsych.finishTrial(trialdata);
+
+    });
 
     // Define start time
     var startTime = performance.now();
