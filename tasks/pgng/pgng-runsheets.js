f331ea8f92f7aee463539799a9ef9e7e73cf9149
karnigili <gili.karni.sun@gmail.com>
2022-09-08 09:55:26 -0400

    add pgng

---
+++ tasks/pgng/pgng-runsheets.js
@@ -0,0 +1,24 @@
+//------------------------------------//
+// Define runsheets.
+//------------------------------------//
+// Runsheet below for demo purposes
+// Email Sam (zorowitz@princeton.edu) for full experiment.
+
+var runsheet_demo = {
+  robots: [
+    [0,1,2,3],
+    [0,1,2,3],
+    [0,1,2,3],
+    [0,1,2,3],
+    [0,1,2,3],
+  ],
+  stimuli: [
+    [ 0, 1, 2, 3],
+    [ 0, 1, 2, 3],
+    [ 0, 1, 2, 3],
+    [ 0, 1, 2, 3],
+    [ 0, 1, 2, 3],
+  ]
+}
+
+var runsheets = [runsheet_demo];
