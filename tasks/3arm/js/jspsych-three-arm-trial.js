7479b5b486cfaec7daf7cbe09eadd5ae9b4093c1
szorowi1 <szorowi1@gmail.com>
2022-11-09 09:10:25 -0500

    3arm branch (1) - move 3arm reversal learning experiment over from https://github.com/nivlab/ThreeArmJS - make compatible w/ jspsych v6.3 - update file paths

---
+++ tasks/3arm/js/jspsych-three-arm-trial.js
@@ -0,0 +1,228 @@
+/**
+ * jspsych-three-arm-trial
+ * Sam Zorowitz
+ *
+ * plugin for running one trial of the fishing 3-arm bandit task
+ *
+ **/
+
+jsPsych.plugins["three-arm-trial"] = (function() {
+
+  var plugin = {};
+
+  plugin.info = {
+    name: 'three-arm-trial',
+    description: '',
+    parameters: {
+      contexts: {
+        type: jsPsych.plugins.parameterType.HTML_STRING,
+        array: true,
+        pretty_name: 'Contexts',
+        description: 'Context marker in order (left, top, right).'
+      },
+      outcomes: {
+        type: jsPsych.plugins.parameterType.INT,
+        array: true,
+        pretty_name: 'Outcomes',
+        description: 'Context outcome if chosen (left, top, right).'
+      },
+      valid_responses: {
+        type: jsPsych.plugins.parameterType.KEYCODE,
+        array: true,
+        pretty_name: 'Choices',
+        default: ['arrowleft', 'arrowup', 'arrowright'],
+        description: 'Keys corresponding to each context (left, top, right).'
+      },
+      choice_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Trial duration',
+        default: null,
+        description: 'How long to show trial before it ends.'
+      },
+      feedback_duration: {
+        type: jsPsych.plugins.parameterType.INT,
+        pretty_name: 'Trial duration',
+        default: 1500,
+        description: 'How long to show feedback before it ends.'
+      }
+    }
+  }
+
+  plugin.trial = function(display_element, trial) {
+
+    // ---------------------------------- //
+    // Section 1: Define HTML             //
+    // ---------------------------------- //
+
+    // Define HTML.
+    var new_html = '';
+
+    // Insert CSS.
+    new_html += `<style>
+    body {
+      height: 100vh;
+      max-height: 100vh;
+      overflow: hidden;
+      position: fixed;
+      background-color: #fdfdfd;
+    }
+    </style>`;
+
+    // Add task container.
+    new_html += '<div class="fishing-wrap">';
+    const sides = ['left','top','right'];
+
+    // Iteratively draw contexts.
+    for (var i = 0; i < 3; i++) {
+
+      // Open beach container.
+      new_html += `<div class="beach-container" id="beach-${sides[i]}" side=${sides[i]}>`;
+
+      // Add landscape.
+      new_html += '<div class="sky"></div>';
+      new_html += '<div class="sand"></div>';
+      new_html += `<div class="wet-sand" context="${trial.contexts[i]}"></div>`;
+      new_html += `<div class="sea" context="${trial.contexts[i]}">`;
+      new_html += `<div class="seafoam" context="${trial.contexts[i]}"></div>`;
+      new_html += '</div>';
+      new_html += '<div class="sand-front"></div>';
+      new_html += `<div class="cloud" context="${trial.contexts[i]}" pattern="1"></div>`;
+      new_html += `<div class="cloud" context="${trial.contexts[i]}" pattern="2"></div>`;
+      new_html += `<div class="cloud" context="${trial.contexts[i]}" pattern="3"></div>`;
+
+      // Add context setters.
+      new_html += `<div class="shadow" side="${sides[i]}"></div>`;
+      new_html += `<div class="surfboard" side="${sides[i]}" context="${trial.contexts[i]}"></div>`;
+      new_html += `<div class="decal" side="${sides[i]}" context="${trial.contexts[i]}"></div>`;
+      new_html += '</div>';
+
+      // End beach.
+      new_html += '</div>';
+
+    }
+
+    // Iteratively draw feedback.
+    for (var i = 0; i < 3; i++) {
+
+      // Add feedback.
+      new_html += `<div class="fish-container" id="fish-${sides[i]}">`;
+      new_html += `<div class="fish" pattern="1" outcome="${trial.outcomes[i]}"></div>`;
+      new_html += `<div class="fish" pattern="3" outcome="${trial.outcomes[i]}"></div>`;
+      new_html += '</div>';
+
+    }
+
+    // Close container.
+    new_html += '</div>';
+
+    // Display HTML.
+    display_element.innerHTML = new_html;
+
+    // ---------------------------------- //
+    // Section 2: jsPsych Functions       //
+    // ---------------------------------- //
+
+    // Preallocate space
+    var response = {
+      rt: null,
+      key: null,
+      choice: null
+    };
+
+    // function to handle responses by the subject
+    var after_response = function(info) {
+
+      // Kill all setTimeout handlers.
+      jsPsych.pluginAPI.clearAllTimeouts();
+      jsPsych.pluginAPI.cancelAllKeyboardResponses();
+
+      // only record the first response
+      if (response.key == null) {
+        response.rt = info.rt;
+        response.key = info.key;
+        response.choice = trial.valid_responses.indexOf(info.key);
+      }
+
+      // Update screen.
+      if (response.choice == 0) {
+        trial.outcome = trial.outcomes[0];
+        display_element.querySelector('#fish-left').setAttribute('context', 'feedback');
+      } else if (response.choice == 1) {
+        trial.outcome = trial.outcomes[1];
+        display_element.querySelector('#fish-top').setAttribute('context', 'feedback');
+      } else {
+        trial.outcome = trial.outcomes[2];
+        display_element.querySelector('#fish-right').setAttribute('context', 'feedback');
+      }
+
+      jsPsych.pluginAPI.setTimeout(function() {
+        end_trial();
+      }, trial.feedback_duration);
+
+    };
+
+    // function to handle missed responses
+    var missed_response = function() {
+
+      // Kill all setTimeout handlers.
+      jsPsych.pluginAPI.clearAllTimeouts();
+      jsPsych.pluginAPI.cancelAllKeyboardResponses();
+
+      // Display warning message.
+      const msg = '<p style="font-size: 20px; line-height: 1.5em">You did not respond within the allotted time. Please pay more attention on the next trial.<br><br><b>Warning:</b> If you miss too many trials, we may end the exepriment early and reject your work.';
+
+      display_element.innerHTML = msg;
+
+      jsPsych.pluginAPI.setTimeout(function() {
+        end_trial();
+      }, 5000);
+
+    }
+
+    // function to end trial when it is time
+    var end_trial = function() {
+
+      // Kill all setTimeout handlers.
+      jsPsych.pluginAPI.clearAllTimeouts();
+      jsPsych.pluginAPI.cancelAllKeyboardResponses();
+
+      // gather the data to store for the trial
+      var trial_data = {
+        "contexts": trial.contexts,
+        "outcomes": trial.outcomes,
+        "key": response.key,
+        "choice": response.choice,
+        "rt": response.rt,
+        "outcome": trial.outcome
+      };
+
+      // clear the display
+      display_element.innerHTML = '';
+
+      // move on to the next trial
+      jsPsych.finishTrial(trial_data);
+
+    };
+
+    // Start the response listener
+    if (trial.valid_responses != jsPsych.NO_KEYS) {
+      var keyboardListener = jsPsych.pluginAPI.getKeyboardResponse({
+        callback_function: after_response,
+        valid_responses: trial.valid_responses,
+        rt_method: 'performance',
+        persist: false,
+        allow_held_key: false
+      });
+    }
+
+    // End trial if no response.
+    if (trial.choice_duration !== null) {
+      jsPsych.pluginAPI.setTimeout(function() {
+        missed_response();
+      }, trial.choice_duration);
+    }
+
+  };
+
+  return plugin;
+})();
---
+++ tasks/3arm/js/three-arm-experiment.js
@@ -0,0 +1,136 @@
+//------------------------------------//
+// Define parameters.
+//------------------------------------//
+
+// Define context (color) assignment.
+const contexts = jsPsych.randomization.shuffle(['red','green','blue']);
+
+// Define choices.
+const choices = ['arrowleft','arrowup','arrowright'];    // left, top, right
+
+// Define timings.
+const choice_duration = null;
+const feedback_duration = 1200;
+
+//------------------------------------//
+// Define images to preload.
+//------------------------------------//
+
+preload_images = [
+  "./img/cloud01.svg",
+  "./img/cloud02.svg",
+  "./img/cloud03.svg",
+  "./img/animal-fish01.svg",
+  "./img/animal-tire.svg",
+  "./img/animal-lion.svg",
+  "./img/animal-snake.svg",
+  "./img/surfboard-blue.svg",
+  "./img/surfboard-green.svg",
+  "./img/surfboard-red.svg",
+  "./img/instructions01.png",
+  "./img/instructions02.png",
+  "./img/instructions03.png",
+  "./img/instructions04.png",
+  "./img/instructions05.png",
+  "./img/instructions06.png",
+  "./img/instructions07.png",
+  "./img/instructions09.png",
+];
+
+//------------------------------------//
+// Define instructions block.
+//------------------------------------//
+
+// Define image scaling CSS.
+const style = "width:auto; height:auto; max-width:100%; max-height:80vh;";
+
+// Define instructions sequence.
+var instructions = {
+  type: 'instructions',
+  pages: [
+    `<img src='./img/instructions01.png' style="${style}"<./img>`,
+    `<img src='./img/instructions02.png' style="${style}"<./img>`,
+    `<img src='./img/instructions03.png' style="${style}"<./img>`,
+    `<img src='./img/instructions04.png' style="${style}"<./img>`,
+    `<img src='./img/instructions05.png' style="${style}"<./img>`,
+    `<img src='./img/instructions06.png' style="${style}"<./img>`,
+    `<img src='./img/instructions07.png' style="${style}"<./img>`,
+    `<img src='./img/instructions09.png' style="${style}"<./img>`,
+  ],
+  show_clickable_nav: true,
+  button_label_previous: "Prev",
+  button_label_next: "Next"
+}
+
+//------------------------------------//
+// Define 3-arm bandit task.
+//------------------------------------//
+
+// Define rotate function.
+function arrayRotate(arr, reverse) {
+  if (reverse) arr.unshift(arr.pop());
+  else arr.push(arr.shift());
+  return arr;
+}
+
+// Preallocate space
+var blocks = [15];    // number of trials
+var probs  = [];
+var outcomes = [];
+var correct = [];
+
+for (var i = 0; i < blocks.length; i++) {
+
+  // Define reward probabilities for block
+  if (i==0) {
+    probs.push(jsPsych.randomization.shuffle([0.8, 0.2, 0.2]));
+  } else if (Math.random() > 0.5) {
+    probs.push(arrayRotate(probs[i-1].slice(), true));
+  } else {
+    probs.push(arrayRotate(probs[i-1].slice(), false));
+  }
+
+  // Iteratively generate outcomes.
+  for (var j = 0; j < blocks[i]; j++) {
+
+    // Store correct choice.
+    correct.push( probs[i].reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0) );
+
+    // Simulate outcomes.
+    var trio = [];
+    for (var k = 0; k < probs[i].length; k++) {
+      trio.push(Math.random() < probs[i][k] ? 1 : 0);
+    }
+    outcomes.push(trio);
+
+  }
+
+}
+
+// Iteratively define trials.
+var trials = [];
+
+for (i = 0; i < outcomes.length; i++) {
+
+  // Define trial.
+  const trial = {
+    type: 'three-arm-trial',
+    contexts: contexts,
+    outcomes: outcomes[i],
+    choices: choices,
+    choice_duration: choice_duration,
+    feedback_duration: feedback_duration,
+    data: { trial: i+1, correct: correct[i] },
+    on_finish: function(data) {
+
+      // Define accuracy.
+      data.accuracy = (data.choice == data.correct ? 1 : 0);
+
+    }
+
+  }
+
+  // Push trial.
+  trials.push(trial);
+
+}
